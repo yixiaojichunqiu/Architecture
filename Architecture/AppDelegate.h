4a1dadd021b249621ed20b033fc5ad5abd9a1cd1
tiankey <785126868@qq.com>
2015-11-26 19:25:39 +0800

    Initial Commit

---
+++ Architecture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Architecture
+//
+//  Created by tarena on 15/11/26.
+//  Copyright © 2015年 weiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Architecture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Architecture
+//
+//  Created by tarena on 15/11/26.
+//  Copyright © 2015年 weiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
